4c245bca252895ca8b12df2a01fe19c59a58e33c
leojay-net <aleonomohjoseph03@gmail.com>
2025-04-27 19:30:14 +0100

    feat: add user profile and web3 authentication features with models, serializers, and views

---
+++ user_profile/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import OrganizationProfileView, RecipientProfileView
+
+router = DefaultRouter()
+router.register(r'organization-profile', OrganizationProfileView, basename='organization-profile')
+router.register(r'recipient-profile', RecipientProfileView, basename='recipient-profile')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
