a023a2ef6e2bdc35d38727feac5dc9434c092d41
web-ghost-dotcom <josephaleonomoh1@gmail.com>
2025-05-23 01:53:44 +0100

    feat(payroll): enhance payroll operations with notification creation for various actions

--- notifications/models.py
+++ notifications/models.py
@@ -1,22 +1,28 @@
 from django.db import models
 from django.conf import settings
 
+
 class Notification(models.Model):
     """
     Model to store notifications for users.
     """
+
     choices = [
-        ('recipientAdded', 'Recipient Added'),
-        ('recipientRemoved', 'Recipient Removed'),
-        ('recipientUpdated', 'Recipient Updated'),
-        ('organizationUpdated', 'Organization Updated'),
-        ('organizationAdded', 'Organization Added'),
-        ('organizationRemoved', 'Organization Removed'),
-        ('login', 'Login'),
-      
-         
+        ("recipientAdded", "Recipient Added"),
+        ("recipientRemoved", "Recipient Removed"),
+        ("recipientUpdated", "Recipient Updated"),
+        ("organizationUpdated", "Organization Updated"),
+        ("organizationAdded", "Organization Added"),
+        ("organizationRemoved", "Organization Removed"),
+        ("login", "Login"),
+        ("payrollCreated", "Payroll Created"),
+        ("payrollUpdated", "Payroll Updated"),
+        ("payrollPaid", "Payroll Paid"),
+        ("payrollDeleted", "Payroll Deleted"),
     ]
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notifications')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notifications"
+    )
     type = models.CharField(max_length=100, choices=choices)
     message = models.TextField()
     is_read = models.BooleanField(default=False)
